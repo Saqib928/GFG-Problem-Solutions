13e30c2a5348ff538eadc5014ea1fc88ecb9aaeb
Mohd Saqib <115813098+Saqib928@users.noreply.github.com>
2025-04-15 16:37:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Index/find-index.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> findIndex(vector<int>& arr, int key) {
+        // code here.
+        vector<int>ans(2,-1);
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==key && ans[0]==-1){
+                ans[0]=i;
+            }
+            if(arr[i]==key){
+                ans[1]=i;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> res;
+        vector<int> arr;
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+        int key;
+        cin >> key;
+        cin.ignore();
+        Solution ob;
+        res = ob.findIndex(arr, key);
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << "\n";
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
